e0404fcf90d728f8264245fe74eebe37c6bf008f
Akin Oluyele <waleander@gmail.com>
2017-06-22 17:57:59 +0100

    add hmr to main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/App.jsx';
+import path from 'path';
+
 
 ReactDOM.render(React.createElement(App),  document.getElementById('container') );
+
+if (module.hot) {
+  module.hot.accept(path.resolve(__dirname, '/components/App'), function() {
+    console.log('Accepting the updated library module!');
+    render(App);
+  })
+}
