4511b488b8015644dac36ac838b2f92d549e0f76
Akin Oluyele <waleander@gmail.com>
2017-06-27 03:57:53 +0100

    fix sourcemap problem in production

--- src/admin-main.js
+++ src/admin-main.js
@@ -7,7 +7,7 @@ import path from 'path';
 ReactDOM.render(React.createElement(App),  document.getElementById('container') );
 
 if (module.hot) {
-  module.hot.accept(path.resolve(__dirname, '/components/App'), function() {
+  module.hot.accept(path.resolve(__dirname, '/components/admin/App'), function() {
     console.log('Accepting the updated library module!');
     ReactDOM.render(App);
   });
