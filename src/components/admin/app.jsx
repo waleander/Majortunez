8375feebba4f702824c15f6a8e5e533107e435e5
Akin Oluyele <waleander@gmail.com>
2017-06-27 04:04:59 +0100

    add some codes to app.jsx in admin

---
+++ src/components/admin/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Majortunez...</h1>
+        <p>watch this space</p>
+        <div className="circles">
+          <div></div>
+          <div></div>
+          <div></div>
+          <span></span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
