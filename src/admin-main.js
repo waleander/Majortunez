4630eb30fb3df20fc95baf7b3a4f459ec184915f
Akin Oluyele <waleander@gmail.com>
2017-06-29 02:46:00 +0100

    fix Admin import

--- src/admin-main.js
+++ src/admin-main.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './components/admin/App.jsx';
+import App from './components/admin/Admin.jsx';
 import path from 'path';
 
 
