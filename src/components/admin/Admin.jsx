b2c66f2265a30451e7196ddac3e6db975d9f94c0
Akin Oluyele <waleander@gmail.com>
2017-06-29 02:37:06 +0100

    rename App to Admin

---
+++ src/components/admin/Admin.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Majortunez...</h1>
+        <p>The admin</p>
+        <div className="circles">
+          <div></div>
+          <div></div>
+          <div></div>
+          <span></span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
